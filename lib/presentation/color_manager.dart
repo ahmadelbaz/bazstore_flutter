0e56ebb0f42208b77beea061db430534d032b5bd
ahmadelbaz <ahmeed.elbaz@gmail.com>
2024-03-22 22:12:23 +0200

    Update color, font, and theme constants
    Update color constants for grey2, grey3, and darkGrey in ColorManager.
    Add new font size constant s11 in FontSize.
    Update theme constants in ThemeManager for card, app bar, button, elevated button,
    and text themes. Add new text styles for different text sizes.

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -7,8 +7,9 @@ class ColorManager {
   static const Color dark = Color(0xFF223263);
   static const Color grey = Color(0xFF9098B1);
   static const Color grey1 = Color(0xff707070);
-  static const Color grey2 = Color(0xff797979);
-  static const Color grey3 = Color(0xffEEEEEE);
+  static const Color grey2 = Color(0xff737477);
+  static const Color grey3 = Color(0xff9e9e9e);
+  static const Color darkGrey = Color(0xff525252);
   static const Color white = Colors.white;
   static const Color black = Colors.black;
   static const Color red = Colors.red;
--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,73 +6,110 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // Main Colors
+    // Main Colors
 
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.white,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1,
-      splashColor: ColorManager.light,
-      fontFamily: FontConstants.fontFamily,
-      colorScheme: const ColorScheme(
-        primary: ColorManager.primary, // Done
-        surface: ColorManager.white, // Done
-        background: ColorManager.white,
-        brightness: Brightness.light, // Done
-        error: ColorManager.red,
-        onBackground: ColorManager.light, // Done
-        onError: Colors.purple,
-        onPrimary: ColorManager.white, // Done
-        onSecondary: Colors.white, // Done
-        onSurface: ColorManager.grey, // Done
-        secondary: ColorManager.darkPrimary,
-      ),
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.white,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    splashColor: ColorManager.light,
+    fontFamily: FontConstants.fontFamily,
+    colorScheme: const ColorScheme(
+      primary: ColorManager.primary, // Done
+      surface: ColorManager.white, // Done
+      background: ColorManager.white,
+      brightness: Brightness.light, // Done
+      error: ColorManager.red,
+      onBackground: ColorManager.light, // Done
+      onError: Colors.purple,
+      onPrimary: ColorManager.white, // Done
+      onSecondary: Colors.white, // Done
+      onSurface: ColorManager.grey, // Done
+      secondary: ColorManager.darkPrimary,
+    ),
 
-      // Card view theme
+    // Card view theme
 
-      cardTheme: const CardTheme(
+    cardTheme: const CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSizes.s4,
+    ),
+    // App bar theme
+    appBarTheme: AppBarTheme(
+      backgroundColor: ColorManager.primary,
+      centerTitle: true,
+      color: ColorManager.white,
+      elevation: AppSizes.s4,
+      shadowColor: ColorManager.light,
+      titleTextStyle: getRegularTextStyle(
+        fontSize: FontSize.s16,
         color: ColorManager.white,
-        shadowColor: ColorManager.grey,
-        elevation: AppSizes.s4,
       ),
-      // App bar theme
-      appBarTheme: AppBarTheme(
+    ),
+
+    // Button theme
+    buttonTheme: ButtonThemeData(
+      buttonColor: ColorManager.primary,
+      focusColor: ColorManager.darkPrimary,
+      hoverColor: ColorManager.light,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(AppSizes.s12),
+      ),
+    ),
+
+    // Elevated Button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
         backgroundColor: ColorManager.primary,
-        centerTitle: true,
-        color: ColorManager.white,
-        elevation: AppSizes.s4,
-        shadowColor: ColorManager.light,
-        titleTextStyle: getRegularTextStyle(
+        textStyle: getRegularTextStyle(
           fontSize: FontSize.s16,
           color: ColorManager.white,
         ),
-      ),
-
-      // Button theme
-      buttonTheme: ButtonThemeData(
-        buttonColor: ColorManager.primary,
-        focusColor: ColorManager.darkPrimary,
-        hoverColor: ColorManager.light,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(AppSizes.s12),
         ),
       ),
+    ),
+    // Text theme
+    textTheme: TextTheme(
+      bodyLarge: getSemiBoldTextStyle(
+        fontSize: FontSize.s18,
+        color: ColorManager.primary,
+      ),
+      bodyMedium: getSemiBoldTextStyle(
+        fontSize: FontSize.s16,
+        color: ColorManager.darkGrey,
+      ),
+      bodySmall: getRegularTextStyle(
+        fontSize: FontSize.s14,
+        color: ColorManager.darkGrey,
+      ),
+      // titleLarge: getSemiBoldTextStyle(
+      //   fontSize: FontSize.s16,
+      // ),
+      titleMedium: getRegularTextStyle(
+        fontSize: FontSize.s14,
+        color: ColorManager.grey2,
+      ),
+      titleSmall: getRegularTextStyle(
+        fontSize: FontSize.s12,
+        color: ColorManager.primary,
+      ),
+      // displayLarge: getBoldTextStyle(
+      //   fontSize: FontSize.s22,
+      // ),
+      displayMedium: getMediumTextStyle(
+        fontSize: FontSize.s12,
+        color: ColorManager.primary,
+      ),
+      displaySmall: getRegularTextStyle(
+        fontSize: FontSize.s11,
+        color: ColorManager.grey2,
+      ),
+    ),
 
-      // Elevated Button theme
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: ColorManager.primary,
-          textStyle: getRegularTextStyle(
-            fontSize: FontSize.s16,
-            color: ColorManager.white,
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(AppSizes.s12),
-          ),
-        ),
-      )
-      // Text theme
-
-      // Input decoration theme
-      );
+    // Input decoration theme
+    inputDecorationTheme: const InputDecorationTheme(),
+  );
 }
